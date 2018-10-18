959f8dc6b6cf4f5c68a362ddb213808186296117
Victorz <victorzsnail@qq.com>
2018-10-18 23:17:17 +0800

    完善第二章

--- src/leaningScala2.scala
+++ src/leaningScala2.scala
@@ -17,5 +17,24 @@ object leaningScala2 {
     println("Froggy went a courting" matches ".* courting")
     println("milk,tea,muck" replaceAll("m[^ ]+k","coffee"))
     println("milk,tea,muck" replaceFirst("m[^ ]+k","coffee"))
+
+
+    //模式匹配
+    val input ="Enjoying this apple 3.141596 times today"
+    val pattern = """.* apple ([\d.]+) times.*""".r
+    val pattern(result)=input
+    //   模式 （输出的结果） = 输入
+    val res=result.toDouble
+    println(res)
+
+    //元组操作
+    val info = (5,"zhang",true)//创建1
+    val red = "red"->"like"//创建2
+
+    //访问元组：
+    val name = info._2
+    val reversed =red._2->red._1
+    println(name)
+    println(reversed)
   }
 }
