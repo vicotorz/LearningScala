437eedb95b3c9fdf840e63da5cd782b327d59121
Victorz <victorzsnail@qq.com>
2018-10-21 15:20:54 +0800

    scala学习手册

--- src/learningScala8.scala
+++ src/learningScala8.scala
@@ -2,7 +2,7 @@
   * Created by dell on 2018/10/21.
   * 第9章  对象、Case类和Trait
   */
-class learningScala8 {
+object learningScala8 {
 
   class A{def hi="hi"}
   trait B{ self:A => override def toString = "B:"+ hi}//增强方法
